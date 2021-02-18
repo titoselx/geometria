2972373aed5865d2123c7f8380d9bc983007e90d
titoselx <74183026+titoselx@users.noreply.github.com>
2021-02-18 21:58:56 +0100

    Versión refactorizada

--- geometria/src/Circulo_ATC.java
+++ geometria/src/Circulo_ATC.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_ATC extends FiguraGeometrica_ATC {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_ATC(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_ATC.java
+++ geometria/src/FiguraGeometrica_ATC.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_ATC {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_ATC(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}

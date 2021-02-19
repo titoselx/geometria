3ccbddb7dffe988549d90c0cd475201577069a95
titoselx <74183026+titoselx@users.noreply.github.com>
2021-02-19 16:20:18 +0100

    Version comentada

--- geometria/src/Rectangulo_ATC.java
+++ geometria/src/Rectangulo_ATC.java
@@ -1,20 +1,32 @@
-
+/**
+ * Clase rectángulo
+ * @author Aarón Titos Checa
+ * @version 1.2
+ */
 public class Rectangulo_ATC extends FiguraGeometrica_ATC {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * Asigna la longitud de los lados
+	 * @param tipoFigura Tipo de la figura
+	 * @param lG Lado 1
+	 * @param lP Lado 2
+	 */
 	public Rectangulo_ATC(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
-	@Override
+/**
+ * 
+ */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}

3783e9662d4687ecdc3966f822e0d53354ea278d
titoselx <74183026+titoselx@users.noreply.github.com>
2021-02-19 16:25:16 +0100

    siempre positivo

--- geometria/src/Circulo_ATC.java
+++ geometria/src/Circulo_ATC.java
@@ -1,11 +1,14 @@
 
 public class Circulo_ATC extends FiguraGeometrica_ATC {
 	private double radio;
-	//Constante pi
+	// Constante pi
 	static final double PI = 3.1416;
-	
+
 	public Circulo_ATC(double r, String tipoFigura) {
 		super(tipoFigura);
+		if (r < 0) {
+			r = r * -1;
+		}
 		radio = r;
 	}
 
@@ -14,7 +17,7 @@ public class Circulo_ATC extends FiguraGeometrica_ATC {
 		return radio * radio * PI;
 
 	}
-	
+
 	@Override
 	public double perimetro() {
 		return radio * 2 * PI;

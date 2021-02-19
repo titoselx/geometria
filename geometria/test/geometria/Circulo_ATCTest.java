12bada4fcd43d137a0c7ab1914a036c30a0baddf
titoselx <74183026+titoselx@users.noreply.github.com>
2021-02-19 18:05:24 +0100

    uff, ahora si

---
+++ geometria/test/geometria/Circulo_ATCTest.java
@@ -0,0 +1,23 @@
+package geometria;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Circulo_ATCTest {
+
+	@Test
+	public void testArea() {
+		assertEquals(7,7);
+		assertEquals(0,0);
+		assertEquals(-3,-3);
+	}
+
+	@Test
+	public void testPerimetro() {
+		assertEquals(7,7);
+		assertEquals(0,0);
+		assertEquals(-3,-3);
+	}
+
+}
